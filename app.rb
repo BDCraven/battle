89732923ea7f220b245588912dca145502f70e70
BDCraven <bencraven@hotmail.com>
2017-07-21 15:01:55 +0100

    Added Player 2 can attack Player 1

--- app.rb
+++ app.rb
@@ -29,7 +29,11 @@ class Battle < Sinatra::Base
   get '/attack' do
     @game = $game
     @game.attack(@game.player_2)
-    @game.switch_turns
     erb :attack
+  end
+
+  post '/switch-turns' do
+    $game.switch_turns
+    redirect '/play'
   end
 end
--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -5,10 +5,18 @@ feature 'Attacking' do
     expect(page).to have_content 'Dave attacked Mittens'
   end
 
+  scenario 'be attacked by Player 2' do
+    sign_in_and_play
+    click_link 'Attack'
+    click_button 'OK'
+    click_link 'Attack'
+    expect(page).to have_content 'Mittens attacked Dave'
+  end
+
   scenario 'reduce Player 2 HP by 10' do
     sign_in_and_play
     click_link 'Attack'
-    # click_link 'OK'
+    click_button 'OK'
     expect(page).not_to have_content 'Mittens: 60HP'
     expect(page).to have_content 'Mittens: 50HP'
   end
